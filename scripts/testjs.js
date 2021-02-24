afd563b74e83a3dfb4e4eadc41fca1528b144007
Fojan Babaali <fozhanmail@gmail.com>
2021-02-24 00:26:06 -0800

    Create testjs.js

---
+++ scripts/testjs.js
@@ -0,0 +1,62 @@
+// jQuery start :
+// nav bar:
+$(function () {
+  $("#item1").click(function () {
+    $("#subItem1")
+      .slideToggle()
+      .css({ "text-align": "center", "font-size": "10px" });
+    $("#subItem1").mouseleave(function () {
+      $("#subItem1").slideUp();
+    });
+  });
+  $("#item2").click(function () {
+    $("#subItem2")
+      .slideToggle()
+      .css({ "text-align": "center", "font-size": "10px" });
+    $("#subItem2").mouseleave(function () {
+      $("#subItem2").slideUp();
+    });
+  });
+  $("#item3").click(function () {
+    $("#subItem3")
+      .slideToggle()
+      .css({ "text-align": "center", "font-size": "10px" });
+    $("#subItem3").mouseleave(function () {
+      $("#subItem3").slideUp();
+    });
+  });
+  $("#item4").click(function () {
+    $("#subItem4")
+      .slideToggle()
+      .css({ "text-align": "center", "font-size": "10px" });
+    $("#subItem4").mouseleave(function () {
+      $("#subItem4").slideUp();
+    });
+  });
+});
+//   Side bar:
+$(function () {
+  $("#sidebarCollapse").click(function () {
+    $(".wrapper").toggle();
+  });
+
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function () {
+    myFunction();
+  };
+
+  // Get the navbar
+  var navbar = document.getElementById("navbar");
+
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+});
